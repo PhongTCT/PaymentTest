552f8fe460021f30aa1bc06e0d9fa4ee755e83c0
NguyenThanh111 <mycrush146@gmail.com>
2026-06-17 23:07:46 +0700

    Update MaintenanceAccessPointDAO.java

--- NetworkManagerWeb/src/java/Models/MaintenanceAccessPointDAO.java
+++ NetworkManagerWeb/src/java/Models/MaintenanceAccessPointDAO.java
@@ -18,6 +18,7 @@ public class MaintenanceAccessPointDAO {
     
 
     public boolean addAP(int maintenanceId, int apId) {
+        String sql = "INSERT INTO MaintenanceAccessPoint (maintenanceId, apId) VALUES (?, ?)";
         try (Connection conn = DbUtils.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setInt(1, maintenanceId);
@@ -30,6 +31,7 @@ public class MaintenanceAccessPointDAO {
     }
 
     public boolean removeAP(int maintenanceId, int apId) {
+        String sql = "DELETE FROM MaintenanceAccessPoint WHERE maintenanceId = ? AND apId = ?";
         try (Connection conn = DbUtils.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setInt(1, maintenanceId);
@@ -43,11 +45,13 @@ public class MaintenanceAccessPointDAO {
 
     public ArrayList<Integer> findAPsByMaintenance(int maintenanceId) {
         ArrayList<Integer> list = new ArrayList<>();
+        String sql = "SELECT apId FROM MaintenanceAccessPoint WHERE maintenanceId = ?";
         try (Connection conn = DbUtils.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setInt(1, maintenanceId);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
+                list.add(rs.getInt("apId"));
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -58,11 +62,13 @@ public class MaintenanceAccessPointDAO {
 
     public ArrayList<Integer> findMaintenancesByAP(int apId) {
         ArrayList<Integer> list = new ArrayList<>();
+        String sql = "SELECT maintenanceId FROM MaintenanceAccessPoint WHERE apId = ?";
         try (Connection conn = DbUtils.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setInt(1, apId);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
+                list.add(rs.getInt("maintenanceId"));
             }
         } catch (Exception e) {
             e.printStackTrace();
